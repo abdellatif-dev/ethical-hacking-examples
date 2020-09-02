899fe536721f3710a677865c15a4711d7691eb36
abdellatif.dev <abdellatif.devlog@gmail.com>
2020-09-02 14:35:21 +0100

    hashcat

--- hashcar.sh
+++
@@ -1,5 +0,0 @@
-#hash-identifier
-  hash-identifier
-
-#md5:
-  hashcat -m 0 hash1_1.txt ~/tools/wordlists/rockyou.txt
--- john.sh
+++ john.sh
@@ -5,4 +5,16 @@
 	sudo john hash.txt --wordlist=~/tools/wordlists/rockyou.txt
 
 #md5:
-	 sudo john -format=raw-md5 --wordlist=~/tools/wordlists/rockyou.txt hash.txt
+	sudo john -format=raw-md5 --wordlist=~/tools/wordlists/rockyou.txt hash.txt
+
+#raw-sha1:
+	sudo john -format=raw-sha1 --wordlist=~/tools/wordlists/rockyou.txt hash.txt
+
+#SHA256:
+	sudo john -format=raw-sha256 --wordlist=~/tools/wordlists/rockyou.txt  hash.txt
+
+#Bcrypt
+	john -format=bcrypt --wordlist=~/tools/wordlists/rockyou.txt  hash.txt
+
+#NTLM:
+	john -format=nt --wordlist=~/tools/wordlists/rockyou.txt  hash.txt
