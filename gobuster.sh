c63242d1735a0ebadc61e82346af8140736ff721
abdellatif.dev <abdellatif.devlog@gmail.com>
2020-10-16 21:11:12 +0000

    first commit

--- gobuster.sh
+++ gobuster.sh
@@ -1,2 +1,2 @@
 #hidden files:
-	gobuster dir -w ~/tools/wordlists/dirbuster/directory-list-2.3-medium.txt  -u $IP
+	gobuster dir -w ~/.tools/wordlists/dirbuster/directory-list-2.3-medium.txt  -u $IP
--- john.sh
+++ john.sh
@@ -2,23 +2,23 @@
   hash-identifier
 
 #cracking:
-	sudo john hash.txt --wordlist=~/tools/wordlists/rockyou.txt
+	sudo john hash.txt --wordlist=~/.tools/wordlists/rockyou.txt
 
 #md5:
-	sudo john -format=raw-md5 --wordlist=~/tools/wordlists/rockyou.txt hash.txt
+	sudo john -format=raw-md5 --wordlist=~/.tools/wordlists/rockyou.txt hash.txt
 
 #raw-sha1:
-	sudo john -format=raw-sha1 --wordlist=~/tools/wordlists/rockyou.txt hash.txt
+	sudo john -format=raw-sha1 --wordlist=~/.tools/wordlists/rockyou.txt hash.txt
 
 #SHA256:
-	sudo john -format=raw-sha256 --wordlist=~/tools/wordlists/rockyou.txt  hash.txt
+	sudo john -format=raw-sha256 --wordlist=~/.tools/wordlists/rockyou.txt  hash.txt
 
 #Bcrypt
-	sudo john -format=bcrypt --wordlist=~/tools/wordlists/rockyou.txt  hash.txt
+	sudo john -format=bcrypt --wordlist=~/.tools/wordlists/rockyou.txt  hash.txt
 
 #NTLM:
-	sudo john -format=nt --wordlist=~/tools/wordlists/rockyou.txt  hash.txt
+	sudo john -format=nt --wordlist=~/.tools/wordlists/rockyou.txt  hash.txt
 
 #SHA512:
-	sudo john --wordlist=~/tools/wordlists/rockyou.txt hash.txt
+	sudo john --wordlist=~/.tools/wordlists/rockyou.txt hash.txt
 	#$6$<Salt>$<hash>
